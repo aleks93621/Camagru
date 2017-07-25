04c951f843ac32f5a06f7ed5e41c353f45aff11f
aaleksov <aleks936@hotmail.fr>
2017-07-25 15:04:47 +0200

    ok

--- connexion_bd.php
+++
@@ -1,8 +0,0 @@
-<?php
-	function connexion()
-	{
-		$pdo = new PDO('mysql:host=localhost;dbname=Camagru', 'root', 'root');
-		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		return ($pdo);
-	}
-?>
